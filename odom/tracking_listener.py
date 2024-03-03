ffe4b853866277495fcf5e8a8a1598156ea0a8f1
Aryamaan <aryamaan.dhomne@gmail.com>
2024-03-03 15:17:38 -0500

    actually fix

--- odom/tracking_listener.py
+++ odom/tracking_listener.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     c = rpyc.connect("localhost", 9001)
     print("Listener Connected")
     while True:
+        pos = c.root.get_position()
         if pos:
-            pos = c.root.get_position()
-        print(pos.get())
+            print(pos.get())
         #s = time.asctime(now)
         #print(s)
         time.sleep(0.1)
